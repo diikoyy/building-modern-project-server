9392183016bc1c465045e656aa000bcc15c56953
Shaun Wassell <shaun@trove.ai>
2019-12-16 11:21:40 -0500

    Basic endpoints and fake data

---
+++ src/dummy-data.js
@@ -0,0 +1,10 @@
+export const fakeTodos = [{
+    message: 'Learn about React Ecosystems',
+    isCompleted: false,
+}, {
+    message: 'Get together with friends',
+    isCompleted: false,
+}, {
+    message: 'Buy groceries',
+    isCompleted: true,
+}];
